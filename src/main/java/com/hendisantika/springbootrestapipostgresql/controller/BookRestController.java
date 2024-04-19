c9ba3eaabed3c9fe0ebf40afc322e1e23626ebcc
geri3313 <geri.meko@fti.edu.al>
2024-04-19 16:07:58 +0200

    Book logging

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
@@ -2,78 +2,82 @@ package com.hendisantika.springbootrestapipostgresql.controller;
 
 import com.hendisantika.springbootrestapipostgresql.entity.Book;
 import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Optional;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-rest-api-postgresql
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 2019-01-18
- * Time: 22:07
- * To change this template use File | Settings | File Templates.
- */
 @RestController
 @RequestMapping("/api/books")
 public class BookRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookRestController.class);
 
     @Autowired
     private BookRepository repository;
 
     @PostMapping
     public ResponseEntity<?> addBook(@RequestBody Book book) {
+        logger.info("Adding new Book: {}", book);
         return new ResponseEntity<>(repository.save(book), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Collection<Book>> getAllBooks() {
+        logger.info("Getting all Books from the repository");
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
-        return new ResponseEntity<Book>(repository.findById(id).get(), HttpStatus.OK);
+        logger.info("Getting Book with id: {}", id);
+        Optional<Book> bookOptional = repository.findById(id);
+        if (bookOptional.isPresent()) {
+            return new ResponseEntity<>(bookOptional.get(), HttpStatus.OK);
+        } else {
+            logger.warn("Book with id: {} not found.", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping(params = {"name"})
     public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "name") String name) {
+        logger.info("Finding Book with name: {}", name);
         return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
-
+        logger.info("Updating Book with id: {}", id);
         Optional<Book> currentBookOpt = repository.findById(id);
-        Book currentBook = currentBookOpt.get();
-        currentBook.setName(book.getName());
-        currentBook.setDescription(book.getDescription());
-        currentBook.setTags(book.getTags());
-
-        return new ResponseEntity<>(repository.save(currentBook), HttpStatus.OK);
+        if (currentBookOpt.isPresent()) {
+            Book currentBook = currentBookOpt.get();
+            currentBook.setName(book.getName());
+            currentBook.setDescription(book.getDescription());
+            currentBook.setTags(book.getTags());
+            return new ResponseEntity<>(repository.save(currentBook), HttpStatus.OK);
+        } else {
+            logger.warn("Book with id: {} not found.", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBookWithId(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteBookWithId(@PathVariable Long id) {
+        logger.info("Deleting Book with id: {}", id);
         repository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping
-    public void deleteAllBooks() {
+    public ResponseEntity<Void> deleteAllBooks() {
+        logger.info("Deleting all Books from the repository");
         repository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
