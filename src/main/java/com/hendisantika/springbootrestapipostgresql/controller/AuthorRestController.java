2697fd2b5d4d5ab75bbefa0fa319a077f46702f5
geri3313 <geri.meko@fti.edu.al>
2024-04-19 12:57:10 +0200

    First commit

---
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
@@ -0,0 +1,84 @@
+package com.hendisantika.springbootrestapipostgresql.controller;
+
+import com.hendisantika.springbootrestapipostgresql.entity.Author;
+import com.hendisantika.springbootrestapipostgresql.repository.AuthorRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorRestController.class);
+
+    @Autowired
+    private AuthorRepository repository;
+
+    @PostMapping
+    public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
+        logger.info("Adding new Author");
+        Author savedAuthor = repository.save(author);
+        return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Author>> getAllAuthors() {
+        logger.info("Getting all Authors from the repository");
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorWithId(@PathVariable Long id) {
+        logger.info("Getting Author with id: {}", id);
+        Optional<Author> authorOptional = repository.findById(id);
+        if (authorOptional.isPresent()) {
+            return new ResponseEntity<>(authorOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(params = {"name"})
+    public ResponseEntity<Collection<Author>> findAuthorWithName(@RequestParam(value = "name") String name) {
+        logger.info("Getting Author with name: {}", name);
+        return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthorFromDB(@PathVariable("id") long id, @RequestBody Author author) {
+        logger.info("Updating Author with id: {}", id);
+        Optional<Author> currentAuthorOpt = repository.findById(id);
+        if (currentAuthorOpt.isPresent()) {
+            Author currentAuthor = currentAuthorOpt.get();
+            currentAuthor.setName(author.getName());
+            currentAuthor.setDescription(author.getDescription());
+            currentAuthor.setTags(author.getTags());
+            Author updatedAuthor = repository.save(currentAuthor);
+            return new ResponseEntity<>(updatedAuthor, HttpStatus.OK);
+        } else {
+            logger.warn("Author with id: {} could not be found.", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthorWithId(@PathVariable Long id) {
+        logger.info("Deleting Author with id: {}", id);
+        repository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllAuthors() {
+        logger.info("Deleting all authors in the repository");
+        repository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
