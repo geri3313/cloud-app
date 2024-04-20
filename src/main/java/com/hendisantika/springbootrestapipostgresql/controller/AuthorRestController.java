40f00ae677a5aff9ac90598a5c3cf333a418e02d
geri3313 <geri.meko@fti.edu.al>
2024-04-20 15:23:33 +0200

    Log4j

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
@@ -2,8 +2,6 @@ package com.hendisantika.springbootrestapipostgresql.controller;
 
 import com.hendisantika.springbootrestapipostgresql.entity.Author;
 import com.hendisantika.springbootrestapipostgresql.repository.AuthorRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +9,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
 
 @RestController
 @RequestMapping("/api/authors")
 public class AuthorRestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthorRestController.class);
+	private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(AuthorRestController.class);
 
     @Autowired
     private AuthorRepository repository;
